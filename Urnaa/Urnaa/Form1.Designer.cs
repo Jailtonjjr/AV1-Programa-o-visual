70ced0fa3439d52d1ef80e4fdb2dfb897b6b2049
leandro <leandrocosta2065@gmail.com>
2025-05-20 11:59:55 -0300

    Add files via upload

---
+++ Urnaa/Urnaa/Form1.Designer.cs
@@ -0,0 +1,447 @@
+﻿namespace Urnaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            numeroUm = new Button();
+            numeroDois = new Button();
+            numeroTres = new Button();
+            numeroQuatro = new Button();
+            numeroSete = new Button();
+            numeroZero = new Button();
+            numeroOito = new Button();
+            numeroCinco = new Button();
+            numeroSeis = new Button();
+            numeroNove = new Button();
+            botaoBranco = new Button();
+            botaoCorrige = new Button();
+            botaoConfirma = new Button();
+            panelPrincipal = new Panel();
+            labelPartido = new Label();
+            labelNome = new Label();
+            fotoPresidente = new PictureBox();
+            textPresidente2 = new TextBox();
+            textPresidente1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panelFim = new Panel();
+            label5 = new Label();
+            relogio = new System.Windows.Forms.Timer(components);
+            panelPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)fotoPresidente).BeginInit();
+            panelFim.SuspendLayout();
+            SuspendLayout();
+            // 
+            // numeroUm
+            // 
+            numeroUm.BackColor = Color.FromArgb(64, 64, 64);
+            numeroUm.FlatStyle = FlatStyle.Popup;
+            numeroUm.ForeColor = Color.White;
+            numeroUm.Location = new Point(549, 162);
+            numeroUm.Name = "numeroUm";
+            numeroUm.Size = new Size(42, 41);
+            numeroUm.TabIndex = 0;
+            numeroUm.Text = "1";
+            numeroUm.TextAlign = ContentAlignment.MiddleLeft;
+            numeroUm.UseVisualStyleBackColor = false;
+            numeroUm.Click += numeroUm_Click;
+            // 
+            // numeroDois
+            // 
+            numeroDois.BackColor = Color.FromArgb(64, 64, 64);
+            numeroDois.FlatStyle = FlatStyle.Popup;
+            numeroDois.ForeColor = Color.White;
+            numeroDois.Location = new Point(604, 162);
+            numeroDois.Name = "numeroDois";
+            numeroDois.Size = new Size(42, 41);
+            numeroDois.TabIndex = 1;
+            numeroDois.Text = "2";
+            numeroDois.TextAlign = ContentAlignment.MiddleLeft;
+            numeroDois.UseVisualStyleBackColor = false;
+            numeroDois.Click += numeroDois_Click;
+            // 
+            // numeroTres
+            // 
+            numeroTres.BackColor = Color.FromArgb(64, 64, 64);
+            numeroTres.FlatStyle = FlatStyle.Popup;
+            numeroTres.ForeColor = Color.White;
+            numeroTres.Location = new Point(659, 162);
+            numeroTres.Name = "numeroTres";
+            numeroTres.Size = new Size(42, 41);
+            numeroTres.TabIndex = 2;
+            numeroTres.Text = "3";
+            numeroTres.TextAlign = ContentAlignment.MiddleLeft;
+            numeroTres.UseVisualStyleBackColor = false;
+            numeroTres.Click += numeroTres_Click;
+            // 
+            // numeroQuatro
+            // 
+            numeroQuatro.BackColor = Color.FromArgb(64, 64, 64);
+            numeroQuatro.FlatStyle = FlatStyle.Popup;
+            numeroQuatro.ForeColor = Color.White;
+            numeroQuatro.Location = new Point(549, 215);
+            numeroQuatro.Name = "numeroQuatro";
+            numeroQuatro.Size = new Size(42, 41);
+            numeroQuatro.TabIndex = 3;
+            numeroQuatro.Text = "4";
+            numeroQuatro.TextAlign = ContentAlignment.MiddleLeft;
+            numeroQuatro.UseVisualStyleBackColor = false;
+            numeroQuatro.Click += numeroQuatro_Click;
+            // 
+            // numeroSete
+            // 
+            numeroSete.BackColor = Color.FromArgb(64, 64, 64);
+            numeroSete.FlatStyle = FlatStyle.Popup;
+            numeroSete.ForeColor = Color.White;
+            numeroSete.Location = new Point(549, 270);
+            numeroSete.Name = "numeroSete";
+            numeroSete.Size = new Size(42, 41);
+            numeroSete.TabIndex = 4;
+            numeroSete.Text = "7";
+            numeroSete.TextAlign = ContentAlignment.MiddleLeft;
+            numeroSete.UseVisualStyleBackColor = false;
+            numeroSete.Click += numeroSete_Click;
+            // 
+            // numeroZero
+            // 
+            numeroZero.BackColor = Color.FromArgb(64, 64, 64);
+            numeroZero.FlatStyle = FlatStyle.Popup;
+            numeroZero.ForeColor = Color.White;
+            numeroZero.Location = new Point(604, 318);
+            numeroZero.Name = "numeroZero";
+            numeroZero.Size = new Size(42, 41);
+            numeroZero.TabIndex = 5;
+            numeroZero.Text = "0";
+            numeroZero.TextAlign = ContentAlignment.MiddleLeft;
+            numeroZero.UseVisualStyleBackColor = false;
+            numeroZero.Click += numeroZero_Click;
+            // 
+            // numeroOito
+            // 
+            numeroOito.BackColor = Color.FromArgb(64, 64, 64);
+            numeroOito.FlatStyle = FlatStyle.Popup;
+            numeroOito.ForeColor = Color.White;
+            numeroOito.Location = new Point(604, 270);
+            numeroOito.Name = "numeroOito";
+            numeroOito.Size = new Size(42, 41);
+            numeroOito.TabIndex = 6;
+            numeroOito.Text = "8";
+            numeroOito.TextAlign = ContentAlignment.MiddleLeft;
+            numeroOito.UseVisualStyleBackColor = false;
+            numeroOito.Click += numeroOito_Click;
+            // 
+            // numeroCinco
+            // 
+            numeroCinco.BackColor = Color.FromArgb(64, 64, 64);
+            numeroCinco.FlatStyle = FlatStyle.Popup;
+            numeroCinco.ForeColor = Color.White;
+            numeroCinco.Location = new Point(604, 215);
+            numeroCinco.Name = "numeroCinco";
+            numeroCinco.Size = new Size(42, 41);
+            numeroCinco.TabIndex = 7;
+            numeroCinco.Text = "5";
+            numeroCinco.TextAlign = ContentAlignment.MiddleLeft;
+            numeroCinco.UseVisualStyleBackColor = false;
+            numeroCinco.Click += numeroCinco_Click;
+            // 
+            // numeroSeis
+            // 
+            numeroSeis.BackColor = Color.FromArgb(64, 64, 64);
+            numeroSeis.FlatStyle = FlatStyle.Popup;
+            numeroSeis.ForeColor = Color.White;
+            numeroSeis.Location = new Point(659, 215);
+            numeroSeis.Name = "numeroSeis";
+            numeroSeis.Size = new Size(42, 41);
+            numeroSeis.TabIndex = 8;
+            numeroSeis.Text = "6";
+            numeroSeis.TextAlign = ContentAlignment.MiddleLeft;
+            numeroSeis.UseVisualStyleBackColor = false;
+            numeroSeis.Click += numeroSeis_Click;
+            // 
+            // numeroNove
+            // 
+            numeroNove.BackColor = Color.FromArgb(64, 64, 64);
+            numeroNove.FlatStyle = FlatStyle.Popup;
+            numeroNove.ForeColor = Color.White;
+            numeroNove.Location = new Point(659, 270);
+            numeroNove.Name = "numeroNove";
+            numeroNove.Size = new Size(42, 41);
+            numeroNove.TabIndex = 9;
+            numeroNove.Text = "9";
+            numeroNove.TextAlign = ContentAlignment.MiddleLeft;
+            numeroNove.UseVisualStyleBackColor = false;
+            numeroNove.Click += numeroNove_Click;
+            // 
+            // botaoBranco
+            // 
+            botaoBranco.BackColor = Color.White;
+            botaoBranco.FlatStyle = FlatStyle.Popup;
+            botaoBranco.ForeColor = Color.Black;
+            botaoBranco.Location = new Point(520, 367);
+            botaoBranco.Name = "botaoBranco";
+            botaoBranco.Size = new Size(65, 40);
+            botaoBranco.TabIndex = 10;
+            botaoBranco.Text = "BRANCO";
+            botaoBranco.TextAlign = ContentAlignment.TopCenter;
+            botaoBranco.UseVisualStyleBackColor = false;
+            botaoBranco.Click += botaoBranco_Click;
+            // 
+            // botaoCorrige
+            // 
+            botaoCorrige.BackColor = Color.FromArgb(255, 128, 0);
+            botaoCorrige.FlatStyle = FlatStyle.Popup;
+            botaoCorrige.ForeColor = Color.Black;
+            botaoCorrige.Location = new Point(594, 367);
+            botaoCorrige.Name = "botaoCorrige";
+            botaoCorrige.Size = new Size(65, 40);
+            botaoCorrige.TabIndex = 11;
+            botaoCorrige.Text = "CORRIGE";
+            botaoCorrige.TextAlign = ContentAlignment.TopCenter;
+            botaoCorrige.UseVisualStyleBackColor = false;
+            botaoCorrige.Click += botaoCorrige_Click;
+            // 
+            // botaoConfirma
+            // 
+            botaoConfirma.BackColor = Color.FromArgb(0, 192, 0);
+            botaoConfirma.FlatStyle = FlatStyle.Popup;
+            botaoConfirma.Font = new Font("Segoe UI", 6.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            botaoConfirma.ForeColor = Color.Black;
+            botaoConfirma.Location = new Point(665, 359);
+            botaoConfirma.Name = "botaoConfirma";
+            botaoConfirma.Size = new Size(65, 48);
+            botaoConfirma.TabIndex = 12;
+            botaoConfirma.Text = "CONFIRMA";
+            botaoConfirma.TextAlign = ContentAlignment.TopCenter;
+            botaoConfirma.UseVisualStyleBackColor = false;
+            botaoConfirma.Click += botaoConfirma_Click;
+            // 
+            // panelPrincipal
+            // 
+            panelPrincipal.BackColor = Color.White;
+            panelPrincipal.BorderStyle = BorderStyle.FixedSingle;
+            panelPrincipal.Controls.Add(labelPartido);
+            panelPrincipal.Controls.Add(labelNome);
+            panelPrincipal.Controls.Add(fotoPresidente);
+            panelPrincipal.Controls.Add(textPresidente2);
+            panelPrincipal.Controls.Add(textPresidente1);
+            panelPrincipal.Controls.Add(label4);
+            panelPrincipal.Controls.Add(label3);
+            panelPrincipal.Controls.Add(label2);
+            panelPrincipal.Controls.Add(label1);
+            panelPrincipal.Location = new Point(39, 107);
+            panelPrincipal.Name = "panelPrincipal";
+            panelPrincipal.Size = new Size(444, 300);
+            panelPrincipal.TabIndex = 13;
+            // 
+            // labelPartido
+            // 
+            labelPartido.AutoSize = true;
+            labelPartido.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelPartido.Location = new Point(141, 176);
+            labelPartido.Name = "labelPartido";
+            labelPartido.Size = new Size(28, 30);
+            labelPartido.TabIndex = 8;
+            labelPartido.Text = "...";
+            labelPartido.Click += labelPartido_Click;
+            // 
+            // labelNome
+            // 
+            labelNome.AutoSize = true;
+            labelNome.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelNome.Location = new Point(116, 145);
+            labelNome.Name = "labelNome";
+            labelNome.Size = new Size(28, 30);
+            labelNome.TabIndex = 7;
+            labelNome.Text = "...";
+            // 
+            // fotoPresidente
+            // 
+            fotoPresidente.BorderStyle = BorderStyle.FixedSingle;
+            fotoPresidente.Location = new Point(294, 52);
+            fotoPresidente.Name = "fotoPresidente";
+            fotoPresidente.Size = new Size(120, 150);
+            fotoPresidente.TabIndex = 6;
+            fotoPresidente.TabStop = false;
+            // 
+            // textPresidente2
+            // 
+            textPresidente2.BackColor = Color.White;
+            textPresidente2.BorderStyle = BorderStyle.FixedSingle;
+            textPresidente2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textPresidente2.Location = new Point(175, 93);
+            textPresidente2.Multiline = true;
+            textPresidente2.Name = "textPresidente2";
+            textPresidente2.Size = new Size(24, 37);
+            textPresidente2.TabIndex = 5;
+            textPresidente2.TextAlign = HorizontalAlignment.Center;
+            textPresidente2.TextChanged += textPresidente2_TextChanged;
+            // 
+            // textPresidente1
+            // 
+            textPresidente1.BackColor = Color.White;
+            textPresidente1.BorderStyle = BorderStyle.FixedSingle;
+            textPresidente1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textPresidente1.Location = new Point(145, 93);
+            textPresidente1.Multiline = true;
+            textPresidente1.Name = "textPresidente1";
+            textPresidente1.Size = new Size(24, 37);
+            textPresidente1.TabIndex = 4;
+            textPresidente1.TextAlign = HorizontalAlignment.Center;
+            textPresidente1.TextChanged += textPresidente1_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(41, 175);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 30);
+            label4.TabIndex = 3;
+            label4.Text = "PARTIDO:";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(37, 100);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 30);
+            label3.TabIndex = 2;
+            label3.Text = "NUMERO";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(41, 145);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 30);
+            label2.TabIndex = 1;
+            label2.Text = "NOME:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 32);
+            label1.TabIndex = 0;
+            label1.Text = "PRESIDENTE";
+            label1.Click += label1_Click;
+            // 
+            // panelFim
+            // 
+            panelFim.BackColor = Color.White;
+            panelFim.BorderStyle = BorderStyle.FixedSingle;
+            panelFim.Controls.Add(label5);
+            panelFim.Location = new Point(39, 107);
+            panelFim.Name = "panelFim";
+            panelFim.Size = new Size(444, 300);
+            panelFim.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(145, 100);
+            label5.Name = "label5";
+            label5.Size = new Size(142, 86);
+            label5.TabIndex = 0;
+            label5.Text = "FIM";
+            label5.Click += label5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelFim);
+            Controls.Add(panelPrincipal);
+            Controls.Add(botaoConfirma);
+            Controls.Add(botaoCorrige);
+            Controls.Add(botaoBranco);
+            Controls.Add(numeroNove);
+            Controls.Add(numeroSeis);
+            Controls.Add(numeroCinco);
+            Controls.Add(numeroOito);
+            Controls.Add(numeroZero);
+            Controls.Add(numeroSete);
+            Controls.Add(numeroQuatro);
+            Controls.Add(numeroTres);
+            Controls.Add(numeroDois);
+            Controls.Add(numeroUm);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Form1";
+            panelPrincipal.ResumeLayout(false);
+            panelPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)fotoPresidente).EndInit();
+            panelFim.ResumeLayout(false);
+            panelFim.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button numeroUm;
+        private Button numeroDois;
+        private Button numeroTres;
+        private Button numeroQuatro;
+        private Button numeroSete;
+        private Button numeroZero;
+        private Button numeroOito;
+        private Button numeroCinco;
+        private Button numeroSeis;
+        private Button numeroNove;
+        private Button botaoBranco;
+        private Button botaoCorrige;
+        private Button botaoConfirma;
+        private Panel panelPrincipal;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label4;
+        private TextBox textPresidente2;
+        private TextBox textPresidente1;
+        private PictureBox fotoPresidente;
+        private Label labelPartido;
+        private Label labelNome;
+        private Panel panelFim;
+        private Label label5;
+        private System.Windows.Forms.Timer relogio;
+    }
+}
